6a3cb62ef399aa1ba90f72de47a9bd41d4bb08be
pytech <onyangodonomondi@gmail.com>
2022-05-02 02:40:27 +0300

    updates

--- dict.py
+++ dict.py
@@ -1,3 +1,6 @@
+import cairo
+
+
 cardict={
     'brand': 'ford',
     'model': 'Tesla',
@@ -6,3 +9,30 @@ cardict={
 print(cardict['brand'])
 print(cardict['model'])
 print(cardict['year'])
+
+# Using the "get method instead of print"
+x=cardict.get('model')
+print(x)
+s=cardict.get('year')
+print(s)
+u=cardict.get('brand')
+print(u)
+
+
+#keys  & Values  NB Note they are keys and not key, values and not value
+
+y=cardict.keys()
+print (y)
+z=cardict.values()
+print(z)
+
+#getting the length of dictionary
+print(len(cardict))
+
+#adding to the dictionary
+
+cardict['color']= 'white'
+
+print(cardict)
+print(cardict.keys())
+print(cardict.values())
